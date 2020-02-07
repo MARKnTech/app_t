8839cb69ebf8bba0fc2a6f8103407c2b0a837ebc
MAd0G <55112409+MAd0G@users.noreply.github.com>
2020-02-07 16:47:18 -0300

    Add files via upload

---
+++ my_app.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.cluster import KMeans
+import streamlit as st 
+
+st.title('My first app')
